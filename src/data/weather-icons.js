165f1cc1028634ea177598aa1f1d26e44fc03e29
jeffkarr <jkarr001@gmail.com>
2024-11-01 13:07:06 -0500

    added CurrentWeather component and weather icons

---
+++ src/data/weather-icons.js
@@ -0,0 +1,102 @@
+import sunny from '../assets/images/weather/sunny.png';
+import cloudy from '../assets/images/weather/cloudy.png';
+import partlyCloudy from '../assets/images/weather/partly-cloudy.png';
+import scatteredShowers from '../assets/images/weather/scattered-showers.png';
+import rain from '../assets/images/weather/rain.png';
+import thunderstorms from '../assets/images/weather/thunderstorms.png';
+import snow from '../assets/images/weather/snow.png';
+const weatherIconsArr = [
+  {
+    "icon_id": "01d",
+    "icon_name": "Clear",
+    "icon_img": sunny
+  },
+  {
+    "icon_id": "02d",
+    "icon_name": "Few Clouds",
+    "icon_img": partlyCloudy
+  },
+  {
+    "icon_id": "03d",
+    "icon_name": "Scattered Clouds",
+    "icon_img": cloudy
+  },
+  {
+    "icon_id": "04d",
+    "icon_name": "Cloudy",
+    "icon_img": cloudy
+  },
+  {
+    "icon_id": "09d",
+    "icon_name": "Drizzle",
+    "icon_img": scatteredShowers
+  },
+  {
+    "icon_id": "10d",
+    "icon_name": "Rain",
+    "icon_img": rain
+  },
+  {
+    "icon_id": "11d",
+    "icon_name": "Thunderstorm",
+    "icon_img": thunderstorms
+  },
+  {
+    "icon_id": "13d",
+    "icon_name": "Snow",
+    "icon_img": snow
+  },
+  {
+    "icon_id": "50d",
+    "icon_name": "Mist",
+    "icon_img": cloudy
+  },
+  {
+    "icon_id": "01n",
+    "icon_name": "Clear",
+    "icon_img": sunny
+  },
+  {
+    "icon_id": "02n",
+    "icon_name": "Few Clouds",
+    "icon_img": partlyCloudy
+  },
+  {
+    "icon_id": "03n",
+    "icon_name": "Scattered Clouds",
+    "icon_img": cloudy
+  },
+  {
+    "icon_id": "04n",
+    "icon_name": "Cloudy",
+    "icon_img": cloudy
+  },
+  {
+    "icon_id": "09n",
+    "icon_name": "Drizzle",
+    "icon_img": scatteredShowers
+  },
+  {
+    "icon_id": "10n",
+    "icon_name": "Rain",
+    "icon_img": rain
+  },
+  {
+    "icon_id": "11n",
+    "icon_name": "Thunderstorm",
+    "icon_img": thunderstorms
+  },
+  {
+    "icon_id": "13n",
+    "icon_name": "Snow",
+    "icon_img": snow
+  },
+  {
+    "icon_id": "50n",
+    "icon_name": "Mist",
+    "icon_img": cloudy
+  }
+];
+
+
+export default weatherIconsArr;
--- src/stores/weatherAPI.js
+++ src/stores/weatherAPI.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useWeatherStore = defineStore('weatherData', () => {
@@ -9,6 +9,14 @@ export const useWeatherStore = defineStore('weatherData', () => {
   let lat = ref("");
   let lon = ref("");
   let currWeatherData = ref({});
+  let currWeatherShortDescr = ref("");  
+  let currWeatherIcon = ref("");
+  let currTemp = ref(0);
+  let currHumidity = ref(0);
+  let currWindSpeed = ref(0);
+  let currWindGust = ref(0);
+  let currWindDirection = ref(0); 
+  let currPressure = ref(0); 
 
   const parseLocData = (locObj) => {
     if (locObj) {
@@ -21,19 +29,33 @@ export const useWeatherStore = defineStore('weatherData', () => {
     };
   };
 
+  const calcPressure = (apiPressure) => {
+    let tempPressure = (apiPressure / 33.8639).toFixed(2);
+    return parseFloat(tempPressure);
+  };
+
   const getCurrentWeather = async () => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
     let apiUrl = `${import.meta.env.VITE_WEATHER_API_URL}?lat=${parseFloat(lat.value)}&lon=${parseFloat(lon.value)}&units=imperial&appid=${import.meta.env.VITE_WEATHER_API_APPID}`;
-
     fetch(apiUrl,requestOptions )
       .then( (response) => response.json() )
-      .then( (result) => { currWeatherData.value = result })
+      .then( (result) => { 
+        currWeatherData.value = Object.assign(result);
+        currTemp.value = parseInt(result.main.temp);
+        currHumidity.value = parseInt(result.main.humidity);
+        currWindSpeed.value = parseInt(result.wind.speed);
+        currWindGust.value = parseInt(result.wind.gust);
+        currWindDirection.value = parseInt(result.wind.deg);  
+        currWeatherShortDescr.value = result.weather[0].main;
+        currWeatherIcon.value = result.weather[0].icon;    
+        currPressure.value = calcPressure(result.main.pressure);
+      })
       .catch( (error) => console.error(error) ); 
-
   };
 
-  return { locationData, locationCityState, locationCoordinates, lat, lon, currWeatherData, parseLocData }
+  return { locationData, locationCityState, locationCoordinates, lat, lon, 
+    currWeatherData, currTemp, currWeatherShortDescr, currWeatherIcon, currWindSpeed, currHumidity, currPressure, parseLocData }
 });
